d16641ce8b5be1e29aabe2977b8936d5c202ca32
Joao Guilherme <joao.guilherme100@hotmail.com>
2022-07-17 16:14:17 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import Header from "./components/Header";
 import CardeVendas from "./components/CardeVendas";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 function App() {
   return (
     <>
+      <ToastContainer />
       <Header />
       <main>
         <section id="vendas">
